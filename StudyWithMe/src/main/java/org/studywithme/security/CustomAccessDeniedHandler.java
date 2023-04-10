a2c40eb5863dc89ca5dff5502b38f816d81632a5
jihun6508 <jihun6508@gmail.com>
2023-04-10 13:45:14 +0900

    로그인 기능 수정, userUtil 클래스 추가(현재 에러, 수정 필요), 권한에 따른 페이지 접근제한 임시 해제

--- StudyWithMe/src/main/java/org/studywithme/security/CustomAccessDeniedHandler.java
+++ StudyWithMe/src/main/java/org/studywithme/security/CustomAccessDeniedHandler.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import lombok.extern.log4j.Log4j;
@@ -14,17 +16,21 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
-  @Override
-  public void handle(HttpServletRequest request, 
-      HttpServletResponse response, AccessDeniedException accessException)
-      throws IOException, ServletException {
+	@Override
+	public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessException)
+			throws IOException, ServletException {
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-    log.error("Access Denied Handler");
+		if (auth != null) {
+			log.warn("User: " + auth.getName() + " attempted to access the protected URL: " + request.getRequestURI());
+		}
+		log.error("Access Denied Handler");
 
-    log.error("Redirect....");
+		log.error("Redirect....");
 
-    response.sendRedirect("/accessError");
+		response.sendRedirect("/accessError");
 
-  }
+	}
 
 }
--- StudyWithMe/src/main/webapp/WEB-INF/views/user/userRoleTest.jsp
+++ StudyWithMe/src/main/webapp/WEB-INF/views/user/userRoleTest.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+<%@ page contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -7,13 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-  
-<h1> Logout Page</h1>
-
-<form action="/logout" method='post'>
-<input type="hidden"name="${_csrf.parameterName}"value="${_csrf.token}"/>
-<button>로그아웃</button>
-</form>
-
+    <h1>User Role Test Result</h1>
+    <p>${result}</p>
 </body>
 </html>
