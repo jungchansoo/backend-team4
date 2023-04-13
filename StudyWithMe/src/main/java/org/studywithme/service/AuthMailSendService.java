14d2136fa02a98ad66775efb72334dd1f48c4aa7
jihun6508 <jihun6508@gmail.com>
2023-04-13 13:24:16 +0900

    인증용 난수 생성 부분 분리

--- StudyWithMe/src/main/java/org/studywithme/service/AuthMailSendService.java
+++ StudyWithMe/src/main/java/org/studywithme/service/AuthMailSendService.java
@@ -1,8 +1,6 @@
 package org.studywithme.service;
 
 public interface AuthMailSendService {
-	public int makeRandomNumber();
-
 	public String sendJoinMail(String userMail);
 
 	public void mailSend(String setFrom, String receiverMail, String title, String content);
