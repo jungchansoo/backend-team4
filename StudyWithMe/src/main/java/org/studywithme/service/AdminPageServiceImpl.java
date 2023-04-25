cefd83398435611dd150e78f08e0fc47beff21fe
subin <whtnqls03@naver.com>
2023-04-25 23:44:23 +0900

    예약목록 검색기능 수정완료.

--- StudyWithMe/src/main/java/org/studywithme/service/AdminPageServiceImpl.java
+++ StudyWithMe/src/main/java/org/studywithme/service/AdminPageServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.studywithme.domain.Criteria;
 import org.studywithme.domain.UserVO;
 import org.studywithme.mapper.MypageMapper;
-import org.studywithme.mapper.UserMapper;
 
 import lombok.AllArgsConstructor;
 
@@ -22,6 +21,7 @@ public class AdminPageServiceImpl implements AdminPageService {
 	public List<UserVO> getListWithPaging(Criteria cri) {
 		return mapper.getListWithPaging(cri);
 	}
+	
 
 	@Override
 	public int getTotal(Criteria cri) {
